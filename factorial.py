f15003ad8db6466a77e7b545773d42fd9574cb6e
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-04-21 12:07:37 -0500

    Agregamos una funcion y un input por el usuario

--- factorial.py
+++ factorial.py
@@ -1,7 +1,11 @@
+def factorial():
+    contador=int(input("Introduce un numero para su factorial: "))
+    i=1
+    num=1
+    while i<=contador:
+        num=i*num
+        i+=1
+    print(f"El factorial de {contador} es: {num}")
 
-i=1
-num=1
-while i<=5:
-    num=i*num
-    print(f"{num}")
-    i+=1
+if __name__=='__main__':
+    factorial()
