b84f2ca494e4c34fa8995e16233e5bdf7811a53c
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-04-21 12:51:52 -0500

    Ciclo alterado de 2 en 2

--- factorial.py
+++ factorial.py
@@ -7,7 +7,7 @@ def factorial():
         i+=1
     print(f"El factorial de {contador} es: {num}")
 def ciclo():
-    for x in range(1,10+1):
+    for x in range(2,10+1):
         print(x, end=" ")
     
 if __name__=='__main__':
